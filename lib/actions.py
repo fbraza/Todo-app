cb3fdba97176e2848bbe6fd7fce9ae642cc18d6b
fbraza <fbraza@tutanota.com>
2020-09-13 20:17:30 +0100

    Refactor Code, lib/Actions: add features

--- lib/actions.py
+++ lib/actions.py
@@ -14,21 +14,15 @@ def create_task(title, priority="Normal", done=False):
 def display_all_tasks(ordered=False, all=False):
     db = plyvel.DB("../taskdb", create_if_missing=True)
     tasks = []
-    # select tasks
+    # select tasks and close db
     if all:
         tasks = [pickle.loads(db.get(key)) for key, _ in db.iterator()]
     else:
         tasks = [pickle.loads(db.get(key)) for key, _ in db.iterator() if not pickle.loads(db.get(key)).done]
     db.close()
-    # sort them
+    # sort tasks if needed
     tasks = sorted(tasks) if ordered else tasks
     system("clear")
     print("------- Your taks -------\n")
     for task in tasks:
         print(task)
-
-
-create_task("Buy Banana", "High")
-create_task("Bake banana cake")
-
-display_all_tasks()
